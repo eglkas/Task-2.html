149e277fb92363610de530c448f52c29352f712f
eglkas <158996561+eglkas@users.noreply.github.com>
2024-04-05 14:00:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+
+var count = 0;
+var output = document.getElementById('output');
+function countClicks()
+{
+    count = count + 1;
+    output.innerHTML = count;
+}
